f4d983048f462705332337708182f7833460bb76
Israr Ahmad <154470627+israrahmad831@users.noreply.github.com>
2025-03-30 22:23:35 +0500

    s

--- Server.js
+++ Server.js
@@ -47,7 +47,10 @@ if (!fs.existsSync(uploadDir)) {
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("DB Connection Error:", err));
 
